78e06bc8671d31ea1d14a40a2f4050287a38ceab
Mario Lépine <Mario.Lepine@ec.gc.ca>
2015-04-30 15:02:19 -0400

    support plus complet du mode 4 + grand menage

--- primitives/constnt.f
+++ primitives/constnt.f
@@ -18,249 +18,242 @@
 * * Boston, MA 02111-1307, USA.
 * */
 ***S/R CONSTNT  -  LIT LE FICHIER DE CONSTANTES COMMUNES CMC-RPN
-* 
+*
       SUBROUTINE CONSTNT(VALEUR,FLAG,NOM,MODE0)
       IMPLICIT NONE
       REAL VALEUR
       INTEGER FLAG,MODE0
       CHARACTER *(*) NOM
       INTEGER IERROR
-      IF (MODE0 .EQ. 4) THEN
-        PRINT *,'ERROR: CONSTNT CALLED WITH MODE0=4 NOT ALLOWED'
-        RETURN
-      ENDIF
-      CALL CONSTNT_X(VALEUR,FLAG,NOM,MODE0,
-     %               -1,-1,-1,-1,IERROR)
+      CALL CONSTNT_X(VALEUR,FLAG,NOM,MODE0,dummy,-1,-1,-1,IERROR)
       RETURN
+      contains
+      subroutine dummy()  ! to support a mode 4 call
+      return
+      end subroutine dummy
       END
       SUBROUTINE CONSTNT_X(VALEUR,FLAG,NOM,MODE0,
-     %                     MPI_BCAST,DATATYPE,ROOT,COMM,IERROR)
-* 
-      IMPLICIT NONE 
-      REAL VALEUR 
+     %                     BCAST_MPI,DATATYPE,ROOT,COMM,IERROR)
+*
+      IMPLICIT NONE
+      REAL VALEUR
       INTEGER FLAG,MODE0,DATATYPE,ROOT,COMM,IERROR
       CHARACTER *(*) NOM
-      EXTERNAL MPI_BCAST
-* 
+      EXTERNAL BCAST_MPI
+*
 *AUTEURS  N. BRUNET ET M. VALIN - FEV 91
-* 
+*
 *REVISION 001 G. PELLERIN - OCTOBRE 1991 - ADAPTATION A unix
 *         002 M.VALIN aller chercher le fichier de constantes
 *             operationnel par defaut suf si fichier "constantes"
 *             present localement  AVRIL 93
 *         003 M.VALIN janvier 2000 ajout d'un mode MPI avec
 *             broadcast + quelques corrections
-*         004 M. DESGAGNE - Decembre 2006 - Introduce BLOCK DATA 
+*         004 M. DESGAGNE - Decembre 2006 - Introduce BLOCK DATA
 *             DATA_CONSTNT_X and return a valid flag for section
 *             (NAME(1).EQ.' ')
-* 
-*OBJET(CONSTNT) 
+*         005 M. Valin - Novembre 2014 corrrection de l'appel "broadcast"
+*             l'argument DATATYPE ne sert plus
+*
+*OBJET(CONSTNT)
 *     LIT LE FICHIER DE CONSTANTES COMMUNES CMC-RPN.
 *     LA VALEUR DE L'ARGUMENT "MODE" INDIQUE LE MODE D'UTILISATION
 *     DE LA ROUTINE. L'UTILISATEUR PEUT AVOIR UNE LISTE IMPRIMEE
 *     COMPLETE DU FICHIER, OU PEUT OBTENIR LA VALEUR D'UNE
-*     CONSTANTE OU PEUT AJOUTER OU MODIFIER ,LOCALEMENT AU TRAVAIL, 
+*     CONSTANTE OU PEUT AJOUTER OU MODIFIER ,LOCALEMENT AU TRAVAIL,
 *     UNE CONSTANTE. Ce module peut etre utilise en mode MPI
 *     et propager a ses partenaires les tables.
-* 
-*PARAMETRES 
-*     MAXCNS - NOMBRE MAXIMUM DE CONSTANTES DANS LE FICHIER 
+*
+*PARAMETRES
+*     MAXCNS - NOMBRE MAXIMUM DE CONSTANTES DANS LE FICHIER
       INTEGER MAXCNS
-      PARAMETER(MAXCNS=200) 
-* 
-*FICHIERS 
-*     ACCES AU FICHIER DE CONSTANTES SE FAIT A L'INTERIEUR MEME 
-*     DE LA SOUS-ROUTINE. 
-* 
+      PARAMETER(MAXCNS=200)
+*
+*FICHIERS
+*     ACCES AU FICHIER DE CONSTANTES SE FAIT A L'INTERIEUR MEME
+*     DE LA SOUS-ROUTINE.
+*
 *ARGUMENTS
-*     VALEUR (E/S) - VALEUR DE LA CONSTANTE DEMANDEE, AJOUTEE 
-*                    OU MODIFIEE. 
+*     VALEUR (E/S) - VALEUR DE LA CONSTANTE DEMANDEE, AJOUTEE
+*                    OU MODIFIEE.
 *     FLAG (S) - SI FLAG=0, LA DEMANDE FUT UN INSUCCES, I.E.
-*                LA CONSTANTE N'EXISTE PAS POUR LE CAS OU ON VEUT 
+*                LA CONSTANTE N'EXISTE PAS POUR LE CAS OU ON VEUT
 *                OBTENIR OU MODIFIER UNE VALEUR, OU LA CONSTANTE
 *                QU'ON DESIRE AJOUTER EXISTE DEJA.
-*              - SI FLAG=1, LA DEMANDE FUT UN SUCCES. 
-*     NOM (E) - NOM DE LA CONSTANTE DONT ON DESIRE OBTENIR, 
-*               AJOUTER OU MODIFIER LA VALEUR.
-*               MAXIMUM DE 8 CARACTERES.  EX. 'OMEGA' 
-*     MODE (E) - SI MODE=0, ON VEUT OBTENIR LA VALEUR D'UNE CONSTANTE 
-*              - SI MODE=1, ON VEUT UN IMPRIME COMPLET DU FICHIER 
-*              - SI MODE=2, ON VEUT AJOUTER (LOCALEMENT AU TRAVAIL) 
-*                           UNE CONSTANTE 
+*              - SI FLAG=1, LA DEMANDE FUT UN SUCCES.
+*     NOM (E)  - NOM DE LA CONSTANTE DONT ON DESIRE OBTENIR,
+*                AJOUTER OU MODIFIER LA VALEUR.
+*                MAXIMUM DE 8 CARACTERES.  EX. 'OMEGA'
+*     MODE (E) - SI MODE=0, ON VEUT OBTENIR LA VALEUR D'UNE CONSTANTE
+*              - SI MODE=1, ON VEUT UN IMPRIME COMPLET DU FICHIER
+*              - SI MODE=2, ON VEUT AJOUTER (LOCALEMENT AU TRAVAIL)
+*                           UNE CONSTANTE
 *              - SI MODE=3, ON VEUT MODIFIER (LOCALEMENT AU TRAVAIL)
 *                           UNE CONSTANTE.
 *              - SI MODE=4, ON VEUT PROPAGER LE CONTENU DES TABLES
-*                           VIA MPI_BCAST. L'ARGUMENT MPI_BCAST
-*                           DOIT AVOIR LA SEQUENCE D'APPEL DE LA
-*                           ROUTINE MPI DU MEME NOM.
-* 
+*                           VIA L'ARGUMENT BCAST_MPI. 
+*              par MODE, on entend mod(MODE0,100)
+*              MODE0 >  100 : VALEUR est un REAL*8
+*              MODE0 <= 100 : VALEUR est un REAL*4
+*     BCAST_MPI (E)   L'ARGUMENT BCAST_MPI DOIT ETRE RPN_COMM_BCST_X
+*     DATATYPE,ROOT,COMM (E) :  ne servent plus 
+*     IERROR (S)  code d'erreur renvoye par RPN_COMM_BCST_X
+*
 
-      EXTERNAL FNOM,FCLOS
-      INTEGER FNOM,FCLOS
+      INTEGER, EXTERNAL :: FNOM,FCLOS
       INTEGER MODE
-* 
-*NOTE 
+*
+*NOTE
 *     SI ON AJOUTE UNE CONSTANTE (MODE=2), NE PAS OUBLIER DE
-*     MODIFIER EN CONSEQUENCE LE COMDECK CONSDYN OU CONSPHY 
-*      QUI RESIDE DANS LA LIBRAIRIE 'PHYSIQUE' ET/OU 'DYNAMIQUE', 
+*     MODIFIER EN CONSEQUENCE LE COMDECK CONSDYN OU CONSPHY
+*      QUI RESIDE DANS LA LIBRAIRIE 'PHYSIQUE' ET/OU 'DYNAMIQUE',
 *     AINSI QUE LE PARAMETER (NBRE=..) DANS LA ROUTINE INCTDYN
 *     ET DANS LA ROUTINE INCTPHY (ROUTINE QUI INITIALISE
-*     LE COMMON 'CTESDYN' OU 'CTESPHY'. 
-* 
+*     LE COMMON 'CTESDYN' OU 'CTESPHY'.
+*     avant de proceder a un appel en MODE 4, il faut que le "root pe" ait fait 
+*     un appel en mode 0/1/2/3 pour faire initialiser les tables
+*
+*     ces tours de passe-passe sont necessaires pour eviter une reference 
+*     externe a la librairie MPI, ce qui rendrait sa presence obligatoire
+*     pour tout appel a constnt/constnt_x et constituerait un ennui serieux
+*     pour tout programme non MPI qui se servirait de ces routines
+*
 **
 *-------------------------------------------------------------------
       real *8 valeur8
       pointer (pv8,valeur8)
-      INTEGER I, NCNS, IER, IUNREAD, istrt(2), iend(2),COUNT,dsize
+      INTEGER I, NCNS, IER, IUNREAD, istrt(2), iend(2),COUNT, dsize
       LOGICAL FEXIST
-* 
+*
       REAL*8 VAL(MAXCNS)
-* 
+*
       CHARACTER *8 NAME(MAXCNS), TNAME
       CHARACTER *42 TEXT
-* 
+*
       COMMON/CONSTNT_PDATA/ istrt, NAME, VAL, NCNS, iend
-* 
+      DATA NAME /MAXCNS * ' '/
+*
 *-----------------------------------------------------------------
-      MODE=mod(mode0,100)
-      pv8=loc(valeur)
-      IF(MODE .EQ. 4) THEN
+      MODE = mod(mode0,100)
+      pv8  = loc(valeur)
+      IF (MODE .EQ. 4) THEN
         dsize = ( loc(istrt(2)) - loc(istrt(1)) )        ! taille d'un "integer"
         COUNT = ( loc(iend(1)) - loc(istrt(1)) ) / dsize ! nombre d' "integers" dans /CONSTNT_PDATA/
-        CALL MPI_BCAST(istrt,COUNT,dsize,ROOT,COMM,IERROR)
+        CALL BCAST_MPI(istrt,COUNT,dsize,ROOT,COMM,IERROR)
+!       ca donne  (root et comm seront ignores)
+!       call RPN_COMM_bcst_x(istrt,COUNT,dsize,ROOT,COMM,IERROR)
+!       et par ricochet
+!       call RPN_COMM_bcast (istrt, count*dsize/4, "MPI_INTEGER",0, "grid", err)
         RETURN
       ENDIF
-      IF(NAME(1).EQ.' ')THEN
+      IF (NAME(1).EQ.' ') THEN   ! lire les tables en provenance du fichier approprie
 
          FLAG=0
          IUNREAD=0
          INQUIRE(FILE='./constantes',EXIST=FEXIST)
-         IF(FEXIST)THEN
-         IER=FNOM(IUNREAD,'constantes','FTN+SEQ+FMT',0)
+         IF (FEXIST) THEN
+           IER=FNOM(IUNREAD,'constantes','FTN+SEQ+FMT',0)
          ELSE
-         IER=FNOM(IUNREAD,'@thermoconsts','FTN+SEQ+FMT',0)
+           IER=FNOM(IUNREAD,'@thermoconsts','FTN+SEQ+FMT',0)
          ENDIF
-         if (IER.ne.0) return
-* 
-         IF(MODE.EQ.1)THEN
+         if (IER.ne.0) return  ! pas trouve le fichier de constantes
+*
+         IF (MODE.EQ.1) THEN
             WRITE(6,600)
-600         FORMAT(1H1,10X,'LISTE DES CONSTANTES COMMUNES CMC-RPN', 
-     $             ///) 
             WRITE(6,602)
-602         FORMAT(2X,'NOM',17X,'VALEUR',8X,'DESCRIPTION',10X,
-     $             'UNITE',//)
-         END IF 
-         DO 1 I=1,MAXCNS
+         END IF
 
-
+         DO I=1,MAXCNS
             READ(IUNREAD,'(2X,A8,2X,E20.13,2X,A42)',END=2,err=3)
-
-     $      NAME(I),VAL(I),TEXT 
-            IF(MODE.EQ.1)WRITE(6,605)NAME(I),VAL(I),TEXT
-605         FORMAT(2X,A8,2X,E20.13,2X,A42)
+     $          NAME(I),VAL(I),TEXT
+            IF (MODE.EQ.1) WRITE(6,605)NAME(I),VAL(I),TEXT
             NCNS = I
-1        CONTINUE 
- 2       FLAG=1
+         ENDDO
+ 2       FLAG = 1
 
-
-C2        CLOSE(IUNREAD) 
- 3       IER=FCLOS(IUNREAD)
-* 
-
-         IF(MODE.EQ.1)RETURN
+ 3       IER = FCLOS(IUNREAD)
+*
+         IF (MODE.EQ.1) RETURN
       END IF
-* 
-*     ----  FIN DE L'INITIALISATION DE NAME ET VAL ---- 
-* 
-      TNAME = NOM 
-* 
+*
+*     ----  FIN DE L'INITIALISATION DE NAME ET VAL ----
+*
+      TNAME = NOM
+*
 *     OPTION  MODE=0, ALLER CHERCHER LA VALEUR D'UNE CONSTANTE
-* 
-      IF(MODE.EQ.0)THEN 
-         FLAG = 0 
-         if(mode0.lt.100)then
-         VALEUR = 0 
+*
+      IF (MODE.EQ.0) THEN
+         FLAG = 0
+         if (mode0.lt.100) then
+           VALEUR = 0
          else
-         valeur8=0
+           valeur8=0
          endif
-         DO 10 I=1,NCNS 
-            IF(TNAME.EQ.NAME(I))THEN
-               FLAG = 1 
-               if(mode0.lt.100)then
-               VALEUR = VAL(I)
+         DO I=1,NCNS
+            IF (TNAME.EQ.NAME(I)) THEN
+               FLAG = 1
+               if (mode0.lt.100) then
+                VALEUR = VAL(I)
                else
-               valeur8=val(i)
+                valeur8=val(i)
                endif
-               RETURN 
+               RETURN
             END IF
-10       CONTINUE 
-* 
-*     OPTION  MODE=2, AJOUTER UNE CONSTANTE 
-* 
+         enddo
+*
+*     OPTION  MODE=2, AJOUTER UNE CONSTANTE
+*
       ELSE IF(MODE.EQ.2)THEN
-         FLAG = 0 
+         FLAG = 0
 *        VERIFIER SI CONSTANTE EXISTE DEJA
-         DO 20 I=1,NCNS 
-            IF(TNAME.EQ.NAME(I))THEN
+         DO I=1,NCNS
+            IF (TNAME.EQ.NAME(I)) THEN
                WRITE(6,620)TNAME
 620            FORMAT(/,5X,'**** LA CTE',1X,A8,1X,'EXISTE DEJA',/)
-               STOP 
+               STOP   ! return avec flag = 0 ?
             END IF
-20       CONTINUE 
-         IF(NCNS.LT.MAXCNS)THEN 
+         enddo
+         IF (NCNS.LT.MAXCNS) THEN
             FLAG = 1
-            NCNS = NCNS + 1 
-            if(mode0 .lt.100)then
-            VAL(NCNS) = VALEUR
+            NCNS = NCNS + 1
+            if (mode0 .lt.100) then
+              VAL(NCNS) = VALEUR
             else
-            val(ncns)=valeur8
+              val(ncns)=valeur8
             endif
             NAME(NCNS) = TNAME
-         ELSE 
+         ELSE
             WRITE(6,625)
 625         FORMAT(/,5X,'**** NB DE CONSTANTES DEPASSE LA LIMITE',/)
-            STOP
-         END IF 
-* 
-*     OPTION  MODE=3,  MODIFIER LA VALEUR D'UNE CONSTANTE 
-* 
-      ELSE IF(MODE.EQ.3)THEN
-         FLAG = 0 
+            STOP   ! return avec flag = 0 ?
+         ENDIF
+*
+*     OPTION  MODE=3,  MODIFIER LA VALEUR D'UNE CONSTANTE
+*
+      ELSE IF (MODE.EQ.3) THEN
+         FLAG = 0
 *        VERIFIE SI CONSTANTE EXISTE
-         DO 30 I=1,NCNS 
-            IF(TNAME.EQ.NAME(I))THEN
-               FLAG = 1 
-               if(mode0 .lt.100)then
-               VAL(I) = VALEUR
+         DO I=1,NCNS
+            IF (TNAME.EQ.NAME(I)) THEN
+               FLAG = 1
+               if (mode0 .lt.100) then
+                 VAL(I) = VALEUR
                else
-               val(i)=valeur8
+                 val(i)=valeur8
                endif
-            END IF
-30       CONTINUE 
-         IF(FLAG.EQ.0)THEN
-            WRITE(6,630)TNAME 
-630         FORMAT(/,5X,'**** LA CTE A MODIFIER',1X,A8,1X,
-     $                  'N*EXISTE PAS',/) 
+               exit
+            ENDIF
+         enddo
+         IF (FLAG.EQ.0) THEN
+            WRITE(6,630)TNAME
             STOP
-         END IF 
-* 
-      END IF
-* 
+         ENDIF
+*
+      ENDIF
+*
+600   FORMAT(1H1,10X,'LISTE DES CONSTANTES COMMUNES CMC-RPN',///)
+602   FORMAT(2X,'NOM',17X,'VALEUR',8X,'DESCRIPTION',10X,'UNITE',//)
+605   FORMAT(2X,A8,2X,E20.13,2X,A42)
+630   FORMAT(/,5X,'**** LA CTE A MODIFIER',1X,A8,1X,"N'EXISTE PAS",/)
       RETURN
-      END 
-      BLOCK DATA DATA_CONSTNT_X
-
-      INTEGER MAXCNS
-      PARAMETER(MAXCNS=200) 
-
-      INTEGER NCNS, istrt(2), iend(2)
-* 
-      REAL*8       VAL(MAXCNS)
-      CHARACTER *8 NAME(MAXCNS)
-* 
-
-      COMMON/CONSTNT_PDATA/ istrt, NAME, VAL, NCNS, iend
-* 
-      DATA NAME /MAXCNS * ' '/
-* 
-      END BLOCK DATA DATA_CONSTNT_X
+      END
