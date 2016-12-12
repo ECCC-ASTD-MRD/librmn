3dec4290dea9302698225209b2eedfcadbd10b44
Mario Lépine <Mario.Lepine@.canada.ca>
2016-12-12 10:51:21 -0500

    (M. Valin) Correction, owner_pid et owner_count doivent etre a l'interieur de la region critique

--- base/set_user_lock.F90
+++ base/set_user_lock.F90
@@ -14,28 +14,29 @@
       integer :: current_pid, owner_pid, owner_count
       logical :: ok
 
-!       if (owner_thread < 0) owner_thread = 0
+!     if (owner_thread < 0) owner_thread = 0
       current_pid = c_get_thread_id()
       current_pid = iand(current_pid,x"FFFFFF")   ! keep lower 24 bits (8-31)
-      owner_pid   = iand(owner_thread,x"FFFFFF")  ! lower 24 bits (8-31)
-      owner_count = ishft(owner_thread,-24)       ! bits 1-7
 
-!       print *,'thread=',current_pid,' lock=',lock,' owner_pid=',owner_pid,' count=',owner_count
+!     print *,'thread=',current_pid,' lock=',lock,' owner_pid=',owner_pid,' count=',owner_count
       ok = .false.
       do while(.not. ok)
 !$OMP CRITICAL
+      owner_pid   = iand(owner_thread,x"FFFFFF")  ! lower 24 bits (8-31)
+      owner_count = ishft(owner_thread,-24)       ! bits 1-7
+      owner_count = iand(owner_count,127)
       if(lock) then
-!         print *,'attempt to lock by thread',current_pid
+!       print *,'attempt to lock by thread',current_pid
         if(owner_count == 0 .or. owner_pid == current_pid) then      ! lock is not owned or already owned by this thread, acquire it
           owner_count = owner_count + 1                               ! add one to depth
           owner_thread = current_pid + ishft(owner_count,24)
           ok = .true.
         endif
-!         if(.not. ok) print *,'unsuccessful attempt to lock by thread',current_pid
+!       if(.not. ok) print *,'unsuccessful attempt to lock by thread',current_pid
       else
-!         print *,'attempt to unlock by thread',current_pid
+!       print *,'attempt to unlock by thread',current_pid
         if(current_pid .ne. owner_pid .and. owner_thread > 0) then  ! lock is owned by another thread
-          print *,"ERROR: attempting to release a lock owned by another process"
+          print *,'ERROR: ',current_pid,' attempting to release a lock owned by',owner_pid
           call qqexit(1)
         endif
         owner_count = owner_count - 1                              ! subtract one from depth
@@ -45,7 +46,7 @@
       endif
 !$OMP END CRITICAL
       enddo
-!       if(lock)      print *,'locked by thread',current_pid,' count=',owner_count
-!       if(.not. lock)print *,'unlocked by thread',current_pid,' count=',owner_count
+!     if(lock)      print *,'locked by thread',current_pid,' count=',owner_count
+!     if(.not. lock)print *,'unlocked by thread',current_pid,' count=',owner_count
       return
       end subroutine set_user_lock
