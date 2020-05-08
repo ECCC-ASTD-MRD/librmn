7645319b4c58aa46283a89aba587b525dca8d500
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2020-05-08 12:15:23 -0500

    Initial cmake version.

---
+++ test/test.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "rpnmacros.h"
+#include "rpn_macros_arch.h"
+
+
+extern int f77name(rmnlib_version)(char *rmn,int *print,int len);
+
+int main(void){
+
+   char rmn[128];
+   int print=0;
+
+   f77name(rmnlib_version)(rmn,&print,127);
+   rmn[126]='\0';
+   fprintf(stderr,"%s\n",&rmn[0]);
+}
