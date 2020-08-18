e46196adf832484ed7682eb5b02dd9b70f763ae1
Samuel Gilbert <samuel.gilbert@canada.ca>
2020-08-18 15:25:26 -0400

    Removed reference to movlev since it's really a simple copy

--- src/gmm/gmm_encodemeta.F90
+++ src/gmm/gmm_encodemeta.F90
@@ -1,40 +1,44 @@
 !> Encode/pack type(gmm_metadata) in a basic Fortran type
-!> @author  Stephane Chamberland, 2008-03
-function gmm_encodemeta(F_meta, F_code) result(F_istat)
+integer function gmm_encodemeta(F_meta, F_code)
     use gmm_internals
     implicit none
 
     type(gmm_metadata), intent(in) :: F_meta
     integer, intent(out):: F_code(:)
 
-    integer :: F_istat  !status
+    integer :: i
 
     if (size(F_code) < GMM_META_SIZE) then
-       F_istat = GMM_ERROR
-       return
+        F_istat = GMM_ERROR
+        return
     endif
-    F_istat = GMM_OK
-    ! FIXME movlev is an undefined external!
-    call movlev(F_meta, F_code, GMM_META_SIZE)
+
+    do i = 1, GMM_META_SIZE
+        F_code(i) = F_meta(i)
+    end do
+
+    gmm_encodemeta = GMM_OK
 end function gmm_encodemeta
 
 
 !> Decode/unpack F_code into type(gmm_metadata)
-!> @author  Stephane Chamberland, 2008-03
-function gmm_decodemeta(F_meta, F_code) result(F_istat)
+integer function gmm_decodemeta(F_meta, F_code)
     use gmm_internals
     implicit none
 
     type(gmm_metadata), intent(out):: F_meta
     integer, intent(in) :: F_code(:)
 
-    integer :: F_istat
+    integer :: i
 
     if (size(F_code) < GMM_META_SIZE) then
        F_istat = GMM_ERROR
        return
     endif
-    F_istat = GMM_OK
-    ! FIXME movlev is an undefined external!
-    call movlev(F_code, F_meta, GMM_META_SIZE)
+
+    do i = 1, GMM_META_SIZE
+        F_meta(i) = F_code(i)
+    end do
+
+    gmm_decodemeta = GMM_OK
 end function gmm_decodemeta
