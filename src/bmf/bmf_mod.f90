70aaaddd9981d52b5643988559556a1799629218
Samuel Gilbert <samuel.gilbert@canada.ca>
2020-08-18 12:23:08 -0400

    Minor cleanup to remove compiler warning about tabs

--- src/bmf/bmf_mod.f90
+++ src/bmf/bmf_mod.f90
@@ -16,50 +16,41 @@
 ! * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ! * Boston, MA 02111-1307, USA.
 ! */
-!
-!	
-!
-	module bmf_mod
-	save
-	type bmf_entree
-  	sequence
-  	  character*4 :: nom
-  	  integer :: padscrap,ni,istart,iend,nj,jstart,jend,nk,kstart,kend
-  	  integer :: time1,time2
-  	  integer :: hgrid,vgrid,dtyp,scat
-  	  integer :: ndata
-  	  integer, dimension(:,:,:), pointer :: tableau  
-  	end type bmf_entree
 
- 
-	type bmf_liste
-	sequence
-	  type(bmf_entree) bmf_champ
-	  type(bmf_liste), pointer :: champ_suivant
-	end type bmf_liste
-
-        integer bmf_intsize
-        integer bmf_realsize
-	integer bmf_real4
-	integer bmf_real8
-	integer bmf_integer4
-	integer bmf_integer8
-	integer bmf_complex8
-	integer bmf_complex16
-        integer bmf_character
-	parameter(bmf_real4=41)
-	parameter(bmf_real8=81)
-	parameter(bmf_integer4=40)
-	parameter(bmf_integer8=80)
-	parameter(bmf_complex8=82)
-	parameter(bmf_complex16=162)
-	parameter(bmf_character=10)
-	integer bmf_length
-	logical bmf_started
-	logical bmf_liste_started
-	integer bmf_errorlvl	
-	
-        type(bmf_liste), pointer :: liste
+module bmf_mod
+    save
+    type bmf_entree
+        sequence
+        character*4 :: nom
+        integer :: padscrap, ni, istart, iend, nj, jstart, jend, nk, kstart, kend
+        integer :: time1, time2
+        integer :: hgrid, vgrid, dtyp, scat
+        integer :: ndata
+        integer, dimension(:,:,:), pointer :: tableau
+    end type bmf_entree
 
 
-	end module bmf_mod
+    type bmf_liste
+        sequence
+        type(bmf_entree) :: bmf_champ
+        type(bmf_liste), pointer :: champ_suivant
+    end type bmf_liste
+
+    integer :: bmf_intsize
+    integer :: bmf_realsize
+
+    integer, parameter :: bmf_real4 = 41
+    integer, parameter :: bmf_real8 = 81
+    integer, parameter :: bmf_integer4 = 40
+    integer, parameter :: bmf_integer8 = 80
+    integer, parameter :: bmf_complex8 = 82
+    integer, parameter :: bmf_complex16 = 162
+    integer, parameter :: bmf_character = 10
+
+    integer bmf_length
+    logical bmf_started
+    logical bmf_liste_started
+    integer bmf_errorlvl
+
+    type(bmf_liste), pointer :: liste
+end module bmf_mod
