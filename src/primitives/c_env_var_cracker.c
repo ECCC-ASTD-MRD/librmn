ba8a92f61f285f045ec11a3afc3d088835d32de3
Samuel Gilbert <samuel.gilbert@canada.ca>
2020-12-30 15:46:48 -0500

    Fixed build fails with -DDEBUG

--- src/primitives/c_env_var_cracker.c
+++ src/primitives/c_env_var_cracker.c
@@ -27,9 +27,9 @@ void c_env_var_cracker(char *fstoption, void (*user_function)(), char *lang)
 {
   char *token, *option;
   char *delimiter1 = ";", *delimiter2 = "=", *delimiter3 = ", ";
-    
+
   char strings[MAXSTRINGS][MAXLINELENGTH];
-  
+
   char *name, *values, *value;
   int length = 0, index = 0, i = 0, result = 0;
 
@@ -42,18 +42,18 @@ void c_env_var_cracker(char *fstoption, void (*user_function)(), char *lang)
     {
       return;
     }
-   
+
 #ifdef DEBUG
   fprintf(stderr, "option: %s\n", option);
-#endif  
-  
+#endif
+
   /* parse options (extract variable name and values) using ";" separator */
   token = strtok(option, delimiter1);
 
   strncpy(strings[length], token, strlen(token));
   strings[length][strlen(token)] = '\0';
   length++;
- 
+
 
   name = (char *)malloc(MAXLINELENGTH);
   values = (char *)malloc(MAXLINELENGTH);
@@ -65,15 +65,15 @@ void c_env_var_cracker(char *fstoption, void (*user_function)(), char *lang)
       strings[length][strlen(token)] = '\0';
       length++;
     }
-  
+
   for(i = 0; i < length; i++)
     {
       /* parse variable name /value using "=" separator */
       token = strtok(strings[i], delimiter2);
       strncpy(name, token, strlen(token));
-            
-      name[strlen(token)] = '\0'; 
-      
+
+      name[strlen(token)] = '\0';
+
       /* convert key to upper case */
       convert_toupper(name);
       /* remove leading whitespace */
@@ -84,10 +84,10 @@ void c_env_var_cracker(char *fstoption, void (*user_function)(), char *lang)
 #ifdef DEBUG
       fprintf(stderr, "name: %s\n", name);
 #endif
-     
+
       /* parse variable name and values using "=" separator */
       while((token = strtok(null, delimiter2)) != NULL)
-	{  
+	{
 
 	  trimleft(token);
 	  if(strlen(token) == 0)
@@ -97,32 +97,28 @@ void c_env_var_cracker(char *fstoption, void (*user_function)(), char *lang)
 
 	  strncpy(values, token, strlen(token));
 	  values[strlen(token)] = '\0';
-	  
+
 
 #ifdef DEBUG
 	  fprintf(stderr, "Values: %s\n", values);
 #endif
-	  
+
 	  /* remove leading whitespace */
 	  trimleft(values);
 	  /* remove trailing whitespace */
 	  trimright(values);
 
-	  /* check opening and closing parenthesis/quotes */ 
+	  /* check opening and closing parenthesis/quotes */
 	  result = check_start_end_char(values, strlen(values));
-
-#ifdef DEBUG
-	  fprintf(stderr, "white_space_count = %d\n", white_space_count);
-#endif
 
 	  if(result == NONMATCHING)
 	    {
 	      return;
-	    }  
-	  
+	    }
+
 	  if(result >= QUOTES)
 	    {
-	      /* remove quotes */ 
+	      /* remove quotes */
 	      remove_quotes(values);
 	    }
 
@@ -135,7 +131,7 @@ void c_env_var_cracker(char *fstoption, void (*user_function)(), char *lang)
 	    {
 	      call_user_function(name, index, values, lang, user_function);
    	    }
-	  
+
 	  else /* case: multiple values per variable */
 	    {
 	      index = 1;
@@ -144,8 +140,8 @@ void c_env_var_cracker(char *fstoption, void (*user_function)(), char *lang)
 	      token = strtok(values, delimiter3);
 	      strncpy(value, token, strlen(token));
 	      value[strlen(token)] = '\0';
- 
-#ifdef DEBUG	      
+
+#ifdef DEBUG
 	      fprintf(stderr, "Value, without white space and parentheses: %s, token = %s\n", value, token);
 #endif
 
@@ -169,10 +165,10 @@ void c_env_var_cracker(char *fstoption, void (*user_function)(), char *lang)
 		  trimright(value);
 
 		  call_user_function(name, index, value, lang, user_function);
-			  
+
 		}
 	    }
-	  
+
 	}
 
     }
@@ -183,7 +179,7 @@ void c_env_var_cracker(char *fstoption, void (*user_function)(), char *lang)
     free(values);
   if(value)
     free(value);
-  
+
 }
 
 
@@ -197,7 +193,7 @@ static void call_user_function(char *name, int index, char *value, char *lang, v
       fprintf(stderr, " Call user C function\n");
 #endif
       user_function(name, index, value);
-      
+
     }
   else if (strcmp(lang, "F") == 0) /* option: user function in Fortran */
     {
@@ -207,7 +203,7 @@ static void call_user_function(char *name, int index, char *value, char *lang, v
       fp1 = _cptofcd(name, strlen(name));
       fp2 = _cptofcd(value, strlen(value));
       user_function(fp1, &index, fp2, strlen(fp1), strlen(fp2));
-            
+
     }
 }
 
@@ -217,7 +213,7 @@ int check_start_end_char(char *string, int length)
 {
   int space_count = 0;
 
-  /* count leading whitespaces in string */  
+  /* count leading whitespaces in string */
   while(isspace(string[space_count]))
     {
       space_count++;
@@ -230,7 +226,7 @@ int check_start_end_char(char *string, int length)
       else
 	return NONMATCHING;
     }
-  
+
   if(string[length - 1] == ']')
     {
       if(string[space_count] == '[')
@@ -239,7 +235,7 @@ int check_start_end_char(char *string, int length)
       else
 	return NONMATCHING;
     }
-  
+
   if(string[length - 1] == '(')
     {
       if(string[space_count] == ')')
@@ -248,7 +244,7 @@ int check_start_end_char(char *string, int length)
       else
 	return NONMATCHING;
     }
-  
+
   if(string[space_count] == '(' )
     {
       if(string[length - 1] == ')')
@@ -256,7 +252,7 @@ int check_start_end_char(char *string, int length)
       else
 	return NONMATCHING;
     }
-  
+
   if(string[length - 1] == '}')
     {
       if(string[space_count] == '{')
@@ -264,7 +260,7 @@ int check_start_end_char(char *string, int length)
       else
 	return NONMATCHING;
     }
-  
+
   if(string[space_count] == '{')
     {
       if(string[length - 1] == '}')
@@ -272,7 +268,7 @@ int check_start_end_char(char *string, int length)
       else
 	return NONMATCHING;
     }
-  
+
   if(string[length - 1] == '\"')
     {
       if(string[space_count] == '\"')
@@ -280,16 +276,16 @@ int check_start_end_char(char *string, int length)
       else
 	return NONMATCHING;
     }
-  
+
   if(string[space_count] == '\"')
     {
       if(string[length - 1] == '\"')
 	return QUOTES;
       else
 	return NONMATCHING;
-      
+
     }
-  
+
   if(string[length - 1] == '\'')
     {
       if(string[space_count] == '\'')
@@ -297,7 +293,7 @@ int check_start_end_char(char *string, int length)
       else
 	return NONMATCHING;
     }
-  
+
   if(string[space_count] == '\'')
     {
       if(string[length - 1] == '\'')
@@ -305,7 +301,7 @@ int check_start_end_char(char *string, int length)
       else
 	return NONMATCHING;
     }
-  
+
   if(space_count >= 0)
     return space_count;
 
@@ -356,8 +352,8 @@ static void remove_quotes(char * string)
 {
   int counter = 0;
   for(counter = 0; counter < strlen(string) - 2; counter++)
-    string[counter] = string[counter + 1 ]; 
+    string[counter] = string[counter + 1 ];
 
   string[strlen(string) - 2] = '\0';
-  
+
 }
