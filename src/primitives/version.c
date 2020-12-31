6e4e6783a12fa0ba13c8d0319f8bd21e0d940b4a
Samuel Gilbert <samuel.gilbert@canada.ca>
2020-12-31 15:46:14 -0500

    Added EC_ARCH and a little doc to the version info

--- src/primitives/version.c
+++ src/primitives/version.c
@@ -1,4 +1,20 @@
 #define PASTE(a,b) a##b
 #define CAT(a,b) PASTE(a,b)
 
+//! Project version
+//!
+//! This is defined with the PROJECT_NAME and VERSION macros that are defined
+//! by the git_version CMake module in cmake_shared.  This ensures that the
+//! version information is taken from the version management system.
+//!
+//! The symbol name is built by concatenating the CMake PROJECT_NAME variable
+//! with "-version".
 char CAT(PROJECT_NAME,_version)[] = VERSION;
+
+
+#ifndef EC_ARCH
+#define EC_ARCH "Undefined"
+#endif
+
+//! EC_ARCH environment variable value when this was compiled
+char CAT(PROJECT_NAME,_ec_arch)[] = EC_ARCH;
