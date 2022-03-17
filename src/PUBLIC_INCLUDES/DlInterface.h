ECCC-ASTD-MRD/librmn

commit a8cebc5488b5a00a3628b94dfc2c4840ca5e5f84

Code cleanup
- Adressing some compiler warnings
- Removed unused variables

src/fstd98/fst_missing.c
@@ -183,8 +183,6 @@
 #include <stdio.h>
 
 #include <dlfcn.h>
-void *DlOpen(const char *filename, int flag);
-void *DlSym(void *handle, const char *symbol);
 
 #include "qstdir.h"
 
@@ -218,6 +216,8 @@ static void (*set_plugin_missing_value_flags)() = NULL ;
 #pragma weak missing_value_used__ = missing_value_used
 int missing_value_used_();
 int missing_value_used__();
+
+
 //! Define missing values from FST_MISSING_VALUE and MISSING_VALUE_PLUGINS environment variables
 //! \return 1 if MISSING_VALUE_FLAGS is defined, 0 otherwise
 int missing_value_used() {
@@ -268,26 +268,26 @@ int missing_value_used() {
         text = getenv("MISSING_VALUE_PLUGINS");
         if (text != NULL) {
             fprintf(stderr, "INFO: opening plugin library '%s'\n", text);
-            handle = DlOpen(text, RTLD_NOW);
+            handle = dlopen(text, RTLD_NOW);
             if (handle != NULL) {
-                SetMissingValueMapping(1, 1, DlSym(handle, "float_decode"), 0, 0, 0);
-                SetMissingValueMapping(1, 1, DlSym(handle, "double_decode"), 0, 0, 1);
-                SetMissingValueMapping(1, 2, DlSym(handle, "uint_decode"), 0, 0, 0);
-                SetMissingValueMapping(1, 2, DlSym(handle, "ubyte_decode"), 1, 0, 0);
-                SetMissingValueMapping(1, 2, DlSym(handle, "ushort_decode"), 0, 1, 0);
-                SetMissingValueMapping(1, 4, DlSym(handle, "int_decode"), 0, 0, 0);
-                SetMissingValueMapping(1, 4, DlSym(handle, "byte_decode"), 1, 0, 0);
-                SetMissingValueMapping(1, 4, DlSym(handle, "short_decode"), 0, 1, 0);
+                SetMissingValueMapping(1, 1, dlsym(handle, "float_decode"), 0, 0, 0);
+                SetMissingValueMapping(1, 1, dlsym(handle, "double_decode"), 0, 0, 1);
+                SetMissingValueMapping(1, 2, dlsym(handle, "uint_decode"), 0, 0, 0);
+                SetMissingValueMapping(1, 2, dlsym(handle, "ubyte_decode"), 1, 0, 0);
+                SetMissingValueMapping(1, 2, dlsym(handle, "ushort_decode"), 0, 1, 0);
+                SetMissingValueMapping(1, 4, dlsym(handle, "int_decode"), 0, 0, 0);
+                SetMissingValueMapping(1, 4, dlsym(handle, "byte_decode"), 1, 0, 0);
+                SetMissingValueMapping(1, 4, dlsym(handle, "short_decode"), 0, 1, 0);
 
-                SetMissingValueMapping(2, 1, DlSym(handle, "float_encode"), 0, 0, 0);
-                SetMissingValueMapping(2, 1, DlSym(handle, "double_encode"), 0, 0, 1);
-                SetMissingValueMapping(2, 2, DlSym(handle, "uint_encode"), 0, 0, 0);
-                SetMissingValueMapping(2, 2, DlSym(handle, "ubyte_encode"), 1, 0, 0);
-                SetMissingValueMapping(2, 2, DlSym(handle, "ushort_encode"), 0, 1, 0);
-                SetMissingValueMapping(2, 4, DlSym(handle, "int_encode"), 0, 0, 0);
-                SetMissingValueMapping(2, 4, DlSym(handle, "byte_encode"), 1, 0, 0);
-                SetMissingValueMapping(2, 4, DlSym(handle, "short_encode"), 0, 1, 0);
-                set_plugin_missing_value_flags = (void(*)()) DlSym(handle, "set_plugin_missing_value_flags");
+                SetMissingValueMapping(2, 1, dlsym(handle, "float_encode"), 0, 0, 0);
+                SetMissingValueMapping(2, 1, dlsym(handle, "double_encode"), 0, 0, 1);
+                SetMissingValueMapping(2, 2, dlsym(handle, "uint_encode"), 0, 0, 0);
+                SetMissingValueMapping(2, 2, dlsym(handle, "ubyte_encode"), 1, 0, 0);
+                SetMissingValueMapping(2, 2, dlsym(handle, "ushort_encode"), 0, 1, 0);
+                SetMissingValueMapping(2, 4, dlsym(handle, "int_encode"), 0, 0, 0);
+                SetMissingValueMapping(2, 4, dlsym(handle, "byte_encode"), 1, 0, 0);
+                SetMissingValueMapping(2, 4, dlsym(handle, "short_encode"), 0, 1, 0);
+                set_plugin_missing_value_flags = (void(*)()) dlsym(handle, "set_plugin_missing_value_flags");
             } else {
                 fprintf(stderr, "WARNING: plugin library '%s' not found\n", text);
             }
@@ -647,11 +647,15 @@ static void fst_float_decode_missing(float *z, int n)  /* float values */
 
 static void fst_int_decode_missing(int *z, int n)  /* signed ints */
 {
-  int zma, zmi;
+    if (missing_value_used() == 0) return;
+    int zma, zmi;
+    fld_int_anal(z, n, &zma, &zmi);
+    while(n--) {
+        if (*z == zma) *z = int_missing_val;
+        z++;
 
-  if(missing_value_used()==0) return;fld_int_anal(z,n,&zma,&zmi);
-  while(n--) { if(*z==zma) *z=int_missing_val ; z++ ; }
-  return;
+    }
+    return;
 }
 
 static void fst_short_decode_missing(short *z, int n) /* signed shorts */

src/fstd98/burp98.c
@@ -262,8 +262,6 @@ static int burp_nbit_datyp(
     int32_t temp;
     int32_t tblmax;
     int32_t tblmin;
-    int v_nbits = inbits;
-    int v_datyp = *datyp;
 
     /* if transparent mode, nomodifications */
     if (((*datyp==2) && (*nbits == 32)) || (datyp == 0)) return 0;
@@ -444,7 +442,7 @@ int c_mrbadd(
     burp_block_header entete;
     buffer_interface_ptr buf = (buffer_interface_ptr) buffer;
     uint32_t *pos;
-    int inbit, idatyp, nombre, err, temp, left, i, bits_added, bfamho;
+    int inbit, idatyp, nombre, err, left, i, bits_added, bfamho;
     int done,indx;
     uint32_t r_nele, r_nval, r_nt, r_bfam, r_bdesc;
     uint32_t r_btyp, r_nbit, r_bit0, r_datyp;
@@ -512,7 +510,6 @@ int c_mrbadd(
     indx = (buf->nbits) / (8 * sizeof(uint32_t));
     pos = (uint32_t *) &(buf->data[indx]);
     *pos = 0;
-    temp = 0;
     left = 8 * sizeof(uint32_t);
     bits_added = 0;
 
@@ -567,7 +564,6 @@ int c_mrbdel(
     int nelements, datyp, nombre, i;
     burp_block_header entete, *block;
     burp_record *burprec;
-    uint32_t *pos;
 
     if ((number < 1) || (number > buf->buf78.buf8)) {
         sprintf(errmsg, "invalid block number");
@@ -738,7 +734,7 @@ int c_mrbloc(
     int blkno
 ) {
     buffer_interface_ptr buf = (buffer_interface_ptr) buffer;
-    int i, bfamho, bfamdesc, err;
+    int bfamho, bfamdesc;
     int match, mskbtyp, mskfamdesc;
     int r_nele, r_nval, r_nt, r_bfam, r_bdesc, r_btyp, r_nbit, r_bit0, r_datyp;
     int bno;
@@ -783,13 +779,13 @@ int c_mrbloc(
     burprec = (burp_record *) buf->data;
     block = (burp_block_header *) burprec->data;
     match = 0;
-    for (i = blkno; i < buf->buf78.buf8; i++) {
+    for (int i = blkno; i < buf->buf78.buf8; i++) {
         match = ((((bfamdesc ^ block[i].bfamdesc) & mskfamdesc) == 0) &&
                 (((btyp ^ block[i].btyp) & mskbtyp) == 0));
         if (match) {
             if (msg_level <= INFORM) {
                 bno = i + 1;
-                err = c_mrbprm(buf, bno, &r_nele, &r_nval, &r_nt,
+                c_mrbprm(buf, bno, &r_nele, &r_nval, &r_nt,
                         &r_bfam, &r_bdesc, &r_btyp, &r_nbit,
                         &r_bit0, &r_datyp);
                 fprintf(stdout, "MRBLOC - find block #%5d NELE=%5d NVAL=%5d NT=%5d BFAM=%4d BTYP=%4d NBITS=%2d BIT0=%8d DATYP=%1d\n",
@@ -872,7 +868,7 @@ int c_mrbrep(
 ) {
     burp_block_header *block;
     buffer_interface_ptr buf = (buffer_interface_ptr) buffer;
-    int nele, nval, nt, nelements;
+    int nele, nval, nt;
     int nbit, bit0, datyp, bitpos, err, done, i, nnbits;
     int nmots, new_nmots, diff_nmots;
     int new_nbit, new_datyp, nombre;
@@ -890,13 +886,11 @@ int c_mrbrep(
         nele = block[blkno - 1].elem1;
         nval = block[blkno - 1].elem2;
         nt = block[blkno - 1].elem3;
-        nelements = nele;
         done = 0;
     } else {
         nele = block[blkno-1].nele;
         nval = block[blkno-1].nval;
         nt = block[blkno-1].nt;
-        nelements = ((nele-3) > 0) ? nele-3 : 0;
         done = (nele < 3) ? nele : 3;
     }
     nbit = block[blkno-1].nbit + 1;
@@ -956,7 +950,6 @@ int c_mrbxtr(
     int nele, nval, nt, nelements;
     int nbit, bit0, datyp, bitpos, err, done, i, rmask, nnbits, nombre, allones;
     int in_header;
-    uint32_t *pos;
 
     err = 0;
     if ((bkno < 1) || (bkno > buf->buf78.buf8)) {
@@ -1026,7 +1019,6 @@ int c_mrfapp(
     int index_fnom, index, width, nw, end_of_file;
     file_table_entry *fte;
     xdf_record_header *header;
-    seq_dir_keys *seq_entry;
 
     index_fnom = fnom_index(iun);
     if (index_fnom == -1) {
@@ -1121,7 +1113,7 @@ int c_mrfput(
 ) {
     buffer_interface_ptr buf = (buffer_interface_ptr)buffer;
     burp_record *burprec;
-    int err, new_handle;
+    int new_handle;
     int temps, flgs, idtyp, lat, lon, dx, dy, elev, drnd, date;
     int oars, runn, nblk, sup, nsup, xaux, nxaux;
     char stnid[10];
@@ -1130,11 +1122,11 @@ int c_mrfput(
     burprec->info.nblks = buf->buf78.buf8;
 
     new_handle = (handle > 0) ? -handle : handle;
-    err = c_xdfput(iun, new_handle, buf);
+    c_xdfput(iun, new_handle, buf);
     if (msg_level <= INFORM) {
         nsup = 0;
         nxaux = 0;
-        err = c_mrbhdr((uint32_t *)buf, &temps, &flgs, stnid, &idtyp, &lat, &lon,
+        c_mrbhdr((uint32_t *)buf, &temps, &flgs, stnid, &idtyp, &lat, &lon,
                 &dx, &dy, &elev, &drnd, &date, &oars, &runn, &nblk,
                 (uint32_t *)&sup, nsup, (uint32_t *)&xaux, nxaux);
         stnid[9] = '\0';

commit 1f39824d28a37086435f25daf1b67e34369b2001

Fixed dl hyjacking system
Inadvertently broken by commit a8cebc5488b5a00a3628b94dfc2c4840ca5e5f84

src/PUBLIC_INCLUDES/DlInterface.h
@@ -1,5 +1,6 @@
-#ifndef DlInterface_DEFINED
-#define DlInterface_DEFINED
+#ifndef DlInterface_H
+#define DlInterface_H
+
 #include <stdlib.h>
 #include <dlfcn.h>
 /*
@@ -18,4 +19,5 @@ void *DlSym(void *handle, const char *symbol);
 char *DlError(void);
 int DlClose(void *handle);
 void DlRegister(void *open, void *sym, void *error, void *close);
+
 #endif

src/fstd98/fst_missing.c
@@ -182,8 +182,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include <dlfcn.h>
 
+#include <DlInterface.h>
 #include "qstdir.h"
 
 //! -1 not initialized, 1 active, 0 not active
@@ -268,26 +268,26 @@ int missing_value_used() {
         text = getenv("MISSING_VALUE_PLUGINS");
         if (text != NULL) {
             fprintf(stderr, "INFO: opening plugin library '%s'\n", text);
-            handle = dlopen(text, RTLD_NOW);
+            handle = DlOpen(text, RTLD_NOW);
             if (handle != NULL) {
-                SetMissingValueMapping(1, 1, dlsym(handle, "float_decode"), 0, 0, 0);
-                SetMissingValueMapping(1, 1, dlsym(handle, "double_decode"), 0, 0, 1);
-                SetMissingValueMapping(1, 2, dlsym(handle, "uint_decode"), 0, 0, 0);
-                SetMissingValueMapping(1, 2, dlsym(handle, "ubyte_decode"), 1, 0, 0);
-                SetMissingValueMapping(1, 2, dlsym(handle, "ushort_decode"), 0, 1, 0);
-                SetMissingValueMapping(1, 4, dlsym(handle, "int_decode"), 0, 0, 0);
-                SetMissingValueMapping(1, 4, dlsym(handle, "byte_decode"), 1, 0, 0);
-                SetMissingValueMapping(1, 4, dlsym(handle, "short_decode"), 0, 1, 0);
+                SetMissingValueMapping(1, 1, DlSym(handle, "float_decode"), 0, 0, 0);
+                SetMissingValueMapping(1, 1, DlSym(handle, "double_decode"), 0, 0, 1);
+                SetMissingValueMapping(1, 2, DlSym(handle, "uint_decode"), 0, 0, 0);
+                SetMissingValueMapping(1, 2, DlSym(handle, "ubyte_decode"), 1, 0, 0);
+                SetMissingValueMapping(1, 2, DlSym(handle, "ushort_decode"), 0, 1, 0);
+                SetMissingValueMapping(1, 4, DlSym(handle, "int_decode"), 0, 0, 0);
+                SetMissingValueMapping(1, 4, DlSym(handle, "byte_decode"), 1, 0, 0);
+                SetMissingValueMapping(1, 4, DlSym(handle, "short_decode"), 0, 1, 0);
 
-                SetMissingValueMapping(2, 1, dlsym(handle, "float_encode"), 0, 0, 0);
-                SetMissingValueMapping(2, 1, dlsym(handle, "double_encode"), 0, 0, 1);
-                SetMissingValueMapping(2, 2, dlsym(handle, "uint_encode"), 0, 0, 0);
-                SetMissingValueMapping(2, 2, dlsym(handle, "ubyte_encode"), 1, 0, 0);
-                SetMissingValueMapping(2, 2, dlsym(handle, "ushort_encode"), 0, 1, 0);
-                SetMissingValueMapping(2, 4, dlsym(handle, "int_encode"), 0, 0, 0);
-                SetMissingValueMapping(2, 4, dlsym(handle, "byte_encode"), 1, 0, 0);
-                SetMissingValueMapping(2, 4, dlsym(handle, "short_encode"), 0, 1, 0);
-                set_plugin_missing_value_flags = (void(*)()) dlsym(handle, "set_plugin_missing_value_flags");
+                SetMissingValueMapping(2, 1, DlSym(handle, "float_encode"), 0, 0, 0);
+                SetMissingValueMapping(2, 1, DlSym(handle, "double_encode"), 0, 0, 1);
+                SetMissingValueMapping(2, 2, DlSym(handle, "uint_encode"), 0, 0, 0);
+                SetMissingValueMapping(2, 2, DlSym(handle, "ubyte_encode"), 1, 0, 0);
+                SetMissingValueMapping(2, 2, DlSym(handle, "ushort_encode"), 0, 1, 0);
+                SetMissingValueMapping(2, 4, DlSym(handle, "int_encode"), 0, 0, 0);
+                SetMissingValueMapping(2, 4, DlSym(handle, "byte_encode"), 1, 0, 0);
+                SetMissingValueMapping(2, 4, DlSym(handle, "short_encode"), 0, 1, 0);
+                set_plugin_missing_value_flags = (void(*)()) DlSym(handle, "set_plugin_missing_value_flags");
             } else {
                 fprintf(stderr, "WARNING: plugin library '%s' not found\n", text);
             }

src/primitives/DlInterface.c
@@ -1,6 +1,6 @@
-#include <DlInterface.h>
 #include <stdlib.h>
-#include <dlfcn.h>
+
+#include <DlInterface.h>
 /*
   rmnlib interface to the dynamic loading functions
   if this module is compiled without -DLIVE
@@ -14,48 +14,48 @@
 
 static void *_DlOpen_(const char *filename, int flag)
 {
-  return(NULL);
+    return NULL;
 }
-static void *(*P_DlOpen)(const char *,int) = _DlOpen_;
+static void *(*P_DlOpen)(const char *, int) = _DlOpen_;
 void *DlOpen(const char *filename, int flag)
 {
-  return((*P_DlOpen)(filename,flag));
+    return (*P_DlOpen)(filename, flag);
 }
 
 void *_DlSym_(void *handle, const char *symbol)
 {
-  return(NULL);
+    return NULL;
 }
-static void *(*P_DlSym)(void *,const char *) = _DlSym_;
+static void *(*P_DlSym)(void *, const char *) = _DlSym_;
 void *DlSym(void *handle, const char *symbol)
 {
-  return ((*P_DlSym)(handle,symbol));
+    return ((*P_DlSym)(handle, symbol));
 }
 
 char *_DlError_(void)
 {
-  return(ERR_NOT_ACTIVE);
+    return ERR_NOT_ACTIVE;
 }
 static char *(*P_DlError)(void) = _DlError_;
 char *DlError(void)
 {
-  return ((*P_DlError)());
+    return ((*P_DlError)());
 }
 
 int _DlClose_(void *handle)
 {
-  return(-1);
+    return -1;
 }
 static int (*P_DlClose)(void *) = NULL;
 int DlClose(void *handle)
 {
-  return ((*P_DlClose)(handle));
+    return ((*P_DlClose)(handle));
 }
 
 void DlRegister(void *open, void *sym, void *error, void *close)
 {
-  P_DlOpen = (void *(*)(const char *,int)) open;
-  P_DlSym = (void *(*)(void *,const char *)) sym;
-  P_DlError = (char *(*)(void)) error;
-  P_DlClose = (int (*)(void *)) close;
+    P_DlOpen = (void *(*)(const char *, int)) open;
+    P_DlSym = (void *(*)(void *, const char *)) sym;
+    P_DlError = (char *(*)(void)) error;
+    P_DlClose = (int (*)(void *)) close;
 }

src/primitives/DlInterface.h
@@ -1,21 +1,0 @@
-#ifndef DlInterface_DEFINED
-#define DlInterface_DEFINED
-#include <stdlib.h>
-#include <dlfcn.h>
-/*
-  rmnlib interface to the dynamic loading functions
-  if this module is compiled without -DLIVE
-  it provides stubs that return a failure code when called
-  the purpose is to have a default version in the library that does not
-  necessitate -ldl at link time for applications
-  if this module is compiled with -DLIVE
-  it becomes a direct interface do dlopen/dlsym/dlerror/dlclose
-*/
-#define ERR_NOT_ACTIVE "ERROR: this is the dummy dynamic loader\n"
-
-void *DlOpen(const char *filename, int flag);
-void *DlSym(void *handle, const char *symbol);
-char *DlError(void);
-int DlClose(void *handle);
-void DlRegister(void *open, void *sym, void *error, void *close);
-#endif