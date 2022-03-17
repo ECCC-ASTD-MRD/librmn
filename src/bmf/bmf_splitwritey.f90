5365d624db30676cd3784feace7738bac996b7a6
Samuel Gilbert <samuel.gilbert@canada.ca>
2022-03-17 16:16:19 -0400

    Removed deprecated bmf

--- src/bmf/bmf_splitwritey.f90
+++
@@ -1,9 +0,0 @@
-!> \deprecated
-Subroutine bmf_splitwritey
-    print *,'*****************************************************'
-    print *,'WARNING: bmf_splitwritey has been replaced by        '
-    print *,' bmf_splitwritey2 with an additional first argument  '
-    print *,' first argument should be RPN_COMM_split             '
-    print *,'*****************************************************'
-    stop
-end
