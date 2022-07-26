b5dfc365d85d8dda9024d5327262d1a413792700
Samuel Gilbert <samuel.gilbert@canada.ca>
2022-07-26 16:47:03 -0400

    New test; basically a sanity check

---
+++ tests/clib_isalpha.F90
@@ -0,0 +1,13 @@
+program clib_interface
+
+#include <clib_interface.cdk>
+
+    character(len = 1) :: str
+    integer :: res
+
+    str = 's'
+    res = clib_isalpha(str)
+    if (res == 0) then
+        error stop
+    end if
+end program clib_interface
