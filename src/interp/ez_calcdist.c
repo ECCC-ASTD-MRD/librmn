0d4621c9af58afd53f165141581af6f299fb9b99
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2022-12-15 15:01:59 +0000

    Need to define _GNU_SOURCE to get the math constants

--- src/interp/ez_calcdist.c
+++ src/interp/ez_calcdist.c
@@ -18,7 +18,7 @@
  * Boston, MA 02111-1307, USA.
  */
 
-
+#define _GNU_SOURCE
 #include <math.h>
 #include <stdio.h>
 #include <rmn/ezscint.h>
--- src/interp/ez_sincoslatlon.c
+++ src/interp/ez_sincoslatlon.c
@@ -18,6 +18,7 @@
  * Boston, MA 02111-1307, USA.
  */
 
+#define _GNU_SOURCE
 #include <math.h>
 
 #include <rmn/ezscint.h>
