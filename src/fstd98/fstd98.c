fe801745420892a76a2d867eeb26ab67dd5651e0
Vincent Magnoux <vincent.magnoux@ec.gc.ca>
2023-05-04 18:34:22 +0000

    Replaced loop with strncpy

--- src/fstd98/fstd98.c
+++ src/fstd98/fstd98.c
@@ -4640,9 +4640,9 @@ int32_t f77name(fstmsq)(int32_t *f_iun, int32_t *f_mip1, int32_t *f_mip2,
     *f_mip1 = (int32_t) mip1;
     *f_mip2 = (int32_t) mip2;
     *f_mip3 = (int32_t) mip3;
-    for (int i = 0; i < 12 && i < l1; i++) {
-        f_metiket[i] = metiket[i];
-    }
+
+    const int num_chars = Min(12, l1);
+    strncpy(f_metiket, metiket, num_chars);
   }
   return err;
 }
