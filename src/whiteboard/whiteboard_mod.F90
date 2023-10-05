cae5b6d0f928b086638e2fca2db0d5629995c766
M. Valin <mfvalin@gmail.com>
2023-10-05 15:15:11 -0400

    added 3 missing functions

--- src/whiteboard/whiteboard_mod.F90
+++ src/whiteboard/whiteboard_mod.F90
@@ -4,4 +4,23 @@ module whiteboard_mod
   implicit none
 #include <rmn/WhiteBoard_2.hf>
 #undef WHITEBOARD_VERSION
+contains
+  function wb_is_ok(errcode) result(status)
+    implicit none
+    integer, intent(IN), value :: errcode
+    logical :: status
+    status = WB_IS_OK(errcode)
+  end function
+  function wb_is_error(errcode) result(status)
+    implicit none
+    integer, intent(IN), value :: errcode
+    logical :: status
+    status = WB_IS_ERROR(errcode)
+  end function
+  function wb_option_set(options, option) result (status)
+    implicit none
+    integer, intent(IN), value :: options, option
+    logical :: status
+    status = WB_OPTION_SET(options,option)
+  end function
 end module
