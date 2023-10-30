8d4d6654c7e71aa63a9af6e848f78d723bdaff1c
Christopher Subich <Christopher.Subich@canada.ca>
2023-10-30 10:54:47 -0400

    Adjust interpv templates for vscode compatibility
    The mock-template code that uses the C preprocessor to
    define multiple versions of Interp/Extrap functions is
    incompatible with Fortran linting, crashing the linter
    (see https://github.com/fortran-lang/fortls/issues/236)
    because it tries to process the Fortran syntax without
    invoking the preprocessor.
    
    This code adjusts the templates to hold just the subroutine
    body, leaving the `end subroutine` statement for the
    enclosing Fortran file.  This change fixes the linter crash,
    although it still doesn't properly understand the
    subroutines.

--- src/interpv/Interp1D_Linear.F90
+++ src/interpv/Interp1D_Linear.F90
@@ -19,31 +19,81 @@
 
 !version that accepts real(single) arguments, with the extended interface
 subroutine Interp1D_Linear_X  &
+    (numInterpSets, srcNumLevels, destNumLevels, &
+    src_ijDim, dst_ijDim, &
+
+    vLevelSource, stateSource, stateDerivSource, &
+
+    posnDestInSrc, vLevelDestn, stateDestn, stateDerivDestn, &
+
+    extrapEnableDown, extrapEnableUp, &
+    extrapGuideDown, extrapGuideUp, &
+
+    flux, numExtArraysIn, numExtArraysOut, &
+    ExtArraysIn, ExtArraysOut &
+   )
 #define real48 single
 #include "Interp1D_Linear_Body.inc"
 #undef real48
+end subroutine
 
 !version that accepts real(double) arguments, with the extended interface
 subroutine Interp1D_Linear_X8  &
+    (numInterpSets, srcNumLevels, destNumLevels, &
+    src_ijDim, dst_ijDim, &
+
+    vLevelSource, stateSource, stateDerivSource, &
+
+    posnDestInSrc, vLevelDestn, stateDestn, stateDerivDestn, &
+
+    extrapEnableDown, extrapEnableUp, &
+    extrapGuideDown, extrapGuideUp, &
+
+    flux, numExtArraysIn, numExtArraysOut, &
+    ExtArraysIn, ExtArraysOut &
+   )
 #define real48 double
 #include "Interp1D_Linear_Body.inc"
 #undef real48
+end subroutine
 
 
 
 
 !version that accepts real(single) arguments
 subroutine Interp1D_Linear  &
+    (numInterpSets, srcNumLevels, destNumLevels, &
+     src_ijDim, dst_ijDim, &
+
+     vLevelSource, stateSource, stateDerivSource, &
+
+     posnDestInSrc, vLevelDestn, stateDestn, stateDerivDestn, &
+
+     extrapEnableDown, extrapEnableUp, &
+     extrapGuideDown, extrapGuideUp &
+    )
 #define real48 single
 #define DestnFunc Interp1D_Linear_X
 #include "Interp1D_PreX_Shell.inc"
 #undef DestnFunc
 #undef real48
+end subroutine
 
 !version that accepts real(double) arguments
 subroutine Interp1D_Linear8  &
+    (numInterpSets, srcNumLevels, destNumLevels, &
+     src_ijDim, dst_ijDim, &
+
+     vLevelSource, stateSource, stateDerivSource, &
+
+     posnDestInSrc, vLevelDestn, stateDestn, stateDerivDestn, &
+
+     extrapEnableDown, extrapEnableUp, &
+     extrapGuideDown, extrapGuideUp &
+    )
 #define real48 double
 #define DestnFunc Interp1D_Linear_X8
 #include "Interp1D_PreX_Shell.inc"
 #undef DestnFunc
 #undef real48
+end subroutine
