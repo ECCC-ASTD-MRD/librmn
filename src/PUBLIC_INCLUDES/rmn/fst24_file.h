8804de2002ae7ca76a942d5d5af48f14c366163e
Samuel Gilbert <samuel.gilbert@ec.gc.ca>
2024-03-12 20:59:25 +0000

    Fixed const issue

--- src/PUBLIC_INCLUDES/rmn/fst24_file.h
+++ src/PUBLIC_INCLUDES/rmn/fst24_file.h
@@ -56,7 +56,7 @@ int32_t   fst24_find_all(fst_file* file, fst_record* results, const int32_t max_
 
 int32_t   fst24_link(fst_file** files, const int32_t num_files);
 int32_t   fst24_unlink(fst_file* const file);
-int32_t   fst24_print_summary(const fst_file* const file, const fst_record_fields* const fields);
+int32_t   fst24_print_summary(fst_file* const file, const fst_record_fields* const fields);
 int32_t   fst24_checkpoint(const fst_file* const file);
 //! @}
 
