e893c8b9546605523452e8fa614368ccec1b055d
Vivian Lee <vivian.lee@ec.gc.ca>
2024-07-15 21:25:08 +0000

    bug fix in extrap for non-global LATLON input grid (issue RPN-SI/Support#1046)

--- src/interp/ez_defzones.c
+++ src/interp/ez_defzones.c
@@ -50,7 +50,7 @@ switch (Grille[gdrow_in][gdcol_in].grtyp[0])
    break;
 
    case 'L':
-      if (Grille[gdrow_out][gdcol_out].extension == 0)
+      if (Grille[gdrow_in][gdcol_in].extension == 0)
          {
          extrap = EZ_EXTRAP;
          }
