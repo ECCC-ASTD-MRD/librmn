50f8f00977932ec884c70efb2ca02ec839591e3a
Vincent Magnoux <vincent.magnoux@ec.gc.ca>
2024-07-17 15:40:14 +0000

    [tests] Fixed readlx test by ignoring warnings from librmn during output comparison

--- tests/readlx/get_diff.sh
+++ tests/readlx/get_diff.sh
@@ -4,4 +4,6 @@ ptr_expr='[0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-F][0-9A-
 
 test $# -lt 2 && exit 1
 
-diff -B -b <(sed -e 's/\s*'${ptr_expr}'\s*/ /' < ${1}) <(sed -e 's/\s*'${ptr_expr}'\s*/ /' < ${2})
+# Remove pointers, since they change at every execution
+# Remove warnings, since they can depend on the environment
+diff -B -b <(sed -e 's/\s*'${ptr_expr}'\s*/ /' -e 's/^(WARNING) .*$//' < ${1}) <(sed -e 's/\s*'${ptr_expr}'\s*/ /' -e 's/^(WARNING) .*$//' < ${2})
