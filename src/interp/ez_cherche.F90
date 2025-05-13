b0cc6bdf5ce4ad0140a96199a280dbd455e3b5ae
Samuel Gilbert <samuel.gilbert@ec.gc.ca>
2025-05-13 18:28:18 +0000

    Doxygen documentation and clean up

--- src/interp/ez_cherche.F90
+++ src/interp/ez_cherche.F90
@@ -1,72 +1,50 @@
-!/* RMNLIB - Library of useful routines for C and FORTRAN programming
-! * Copyright (C) 1975-2001  Division de Recherche en Prevision Numerique
-! *                          Environnement Canada
-! *
-! * This library is free software; you can redistribute it and/or
-! * modify it under the terms of the GNU Lesser General Public
-! * License as published by the Free Software Foundation,
-! * version 2.1 of the License.
-! *
-! * This library is distributed in the hope that it will be useful,
-! * but WITHOUT ANY WARRANTY; without even the implied warranty of
-! * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-! * Lesser General Public License for more details.
-! *
-! * You should have received a copy of the GNU Lesser General Public
-! * License along with this library; if not, write to the
-! * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-! * Boston, MA 02111-1307, USA.
-! */
-!**function cherche
+! RMNLIB - Library of useful routines for C and FORTRAN programming
+! Copyright (C) 1975-2001  Division de Recherche en Prevision Numerique
+!                          Environnement Canada
 !
-      integer function ez_cherche(val, tableau, nbelem)
-      implicit none
-      real val
-      integer nbelem
-      real tableau(nbelem)
+! This library is free software; you can redistribute it and/or
+! modify it under the terms of the GNU Lesser General Public
+! License as published by the Free Software Foundation,
+! version 2.1 of the License.
 !
-!AUTHOR     Yves Chartier               Jan 1991
+! This library is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+! Lesser General Public License for more details.
 !
-!REVISION
-!     Documentation update              Jul 1994      
-!
-!LANGUAGE   Fortran 77
-!
-!OBJECT (cherche)
-!     Return the index of the table element equal or closest to "val",
-!     using binary search. It is assumed that the table is sorted in ascending
-!     order.
-!
-!FILES
-!
-!ARGUMENTS
-! NAMES          I/O  TYPE  A/S        DESCRIPTION
-! val             O    R     S         value to search for
-! tableau         I    R     A         table of lookup values
-! nbelem          I    I     S         length of table
-!
-!IMPLICIT
-!
-!MODULES
-!
-!*
-      integer debut, milieu, fin
-      
-      debut = 1
-      fin = nbelem
-      milieu = (debut+fin)*0.5
+! You should have received a copy of the GNU Lesser General Public
+! License along with this library; if not, write to the
+! Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+! Boston, MA 02111-1307, USA.
 
-10    if (milieu.ne.debut) then
-          if (val.le.tableau(milieu)) then
-          fin   = milieu
-          else
-          debut = milieu
-      endif
-          milieu = (debut+fin)*0.5
-          goto 10
-      endif
 
-      ez_cherche = milieu
-      
-      return
-      end
+!> \file
+
+
+!> Find the index of the element equal or closest to "val"
+integer function ez_cherche(val, tableau, nbelem)
+    implicit none
+
+    !> Value to search for
+    real, intent(in) :: val
+    !> Number of elements in array
+    integer, intent(in) :: nbelem
+    !> Sorted array in ascending order
+    real, intent(in) :: tableau(nbelem)
+
+    integer :: debut, milieu, fin
+
+    debut = 1
+    fin = nbelem
+    milieu = int((debut + fin) * 0.5)
+
+    do while (milieu /= debut)
+        if (val <= tableau(milieu)) then
+              fin   = milieu
+        else
+              debut = milieu
+        endif
+        milieu = int((debut + fin) * 0.5)
+    end do
+    ez_cherche = milieu
+end
