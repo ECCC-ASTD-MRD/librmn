80e090ba58c9cadf4cdd7aeb7c798aa9f5041a2c
Samuel Gilbert <samuel.gilbert@ec.gc.ca>
2025-09-24 16:55:17 +0000

    Fix missing external, fct as sub, scalar as array

--- src/base/jjulien.f
+++ src/base/jjulien.f
@@ -47,9 +47,10 @@ C     de commentaires (desactivation de code).
 *
       Integer     jour,mois,annee
       Integer     jdebut,jfin,datim(14),is1,is2
+      Integer     dummy, newdate
       Real*8      heures
 *
-      External    datmgp,incdatr,jdatec
+      External    datmgp,incdatr,jdatec, newdate
 *
 *----------------------------------------------------------------
 ***    Calculer le nombre d'heures depuis 
@@ -69,7 +70,7 @@ C     de commentaires (desactivation de code).
 ***    Extraire l'annee, le mois, le jour et l'heure correspondante.
 *
 *     Call datmgp( datim )
-      call newdate(datim(14),is1,is2,-3)
+      dummy = newdate(datim(14), [is1],is2,-3)
 *
 *     heures = datim(5)
       heures = is2/1000000
